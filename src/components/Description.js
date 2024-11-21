4afdaadb7df9d8236667f1bafb8619cbc1a526a5
Lemi Bekele <lemibekele365@gmail.com>
2024-11-21 22:02:20 +0300

    carousle done

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,11 +1,65 @@
-import React from "react";
+import { portfolioProjects } from "@/utils/constants";
+import Image from "next/image";
 
-function Description() {
+function Description({ Prev, Next, activeProject }) {
   return (
-    <div className="grid place-items-start w-full bg-[#e7dfd9] relative">
-      <div className="uppercase text-sm absolute right-4 top-2 underline-offset-4 underline">
-        {" "}
-        Projects
+    <div className="grid place-items-start w-full  relative">
+      {portfolioProjects.map((setNum, idx) => (
+        <div
+          key={idx}
+          className={
+            idx === activeProject ? `text-4xl font-bold absolute ` : `hidden`
+          }
+        >
+          {setNum.id + "."}
+        </div>
+      ))}
+
+      <div className="">
+        {portfolioProjects.map((desc, idx) => (
+          <div
+            key={idx}
+            className={
+              idx === activeProject
+                ? `block w-full h-[80vh] object-cover transition-all duration-500 ease-in-out `
+                : `hidden`
+            }
+          >
+            <div className="py-16 text-5xl font-extrabold">{desc.title}</div>
+            <div className="leading-relaxed font-medium text-base tracking-wide h-60 md:h-40 italic text-gray-600">
+              {desc.description}
+            </div>
+            <button className="w-[9rem] h-[3rem] bg-purple-900 font-bold text-white mt-[1rem]">
+              View Project
+            </button>
+            <div className=" py-2 absolute bottom-1 right-5 flex justify-center items-center gap-4 w-[110px] ">
+              <div
+                onClick={Prev}
+                className="absolute bottom-2 right-10 cursor-pointer"
+              >
+                <Image
+                  className="bg-purple-900 rounded-full "
+                  src=".//left.svg"
+                  width={20}
+                  height={20}
+                  alt=""
+                ></Image>
+              </div>
+              <div
+                onClick={Next}
+                className="absolute bottom-2 right-1 cursor-pointer"
+              >
+                <Image
+                  className="bg-purple-900 rounded-full "
+                  src=".//right.svg"
+                  width={20}
+                  height={20}
+                  alt=""
+                ></Image>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/ProjectCarousel.js
+++ src/components/ProjectCarousel.js
@@ -1,14 +1,45 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { portfolioProjects } from "@/utils/constants.js";
 import Image from "next/image";
 import Description from "./Description";
 
 const ProjectCarousel = () => {
+  const [activeProject, setActiveProject] = useState(0);
+
+  const nextProject = () => {
+    activeProject === portfolioProjects.length - 1
+      ? setActiveProject(0)
+      : setActiveProject(activeProject + 1);
+  };
+  const prevProject = () => {
+    activeProject === 0
+      ? setActiveProject(portfolioProjects.length -1)
+      : setActiveProject(activeProject - 1);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      nextProject();
+    }, 5000);
+    return () => {
+      clearTimeout(timer);
+    };
+  });
   return (
-    <div className="grid place-items-center grid-cols-2 w-full max-w-5xl shadow-2xl mx-auto">
-      <div className="w-full flex justify-center items-center gap-4 transition-transform ease-in-out duration-500">
+    <div className="grid place-items-center md:grid-cols-2 grid-cols-1 w-full max-w-5xl shadow-2xl mx-auto gap-5">
+      <div
+        className={`w-full flex justify-center items-center gap-4 transition-transform ease-in-out duration-500 p-6 md:p-0`}
+      >
         {portfolioProjects.map((pic, idx) => (
-          <div key={idx}>
+          <div
+            className={
+              idx === activeProject
+                ? `block w-full h-[80vh] object-cover transition-all duration-500 ease-in-out`
+                : "hidden"
+            }
+            key={idx}
+          >
             <Image
               src={pic.image}
               alt=""
@@ -18,7 +49,13 @@ const ProjectCarousel = () => {
             ></Image>
           </div>
         ))}
-        <Description />
+      </div>
+      <div>
+        <Description
+          activeProject={activeProject}
+          Next={nextProject}
+          Prev={prevProject}
+        />
       </div>
     </div>
   );
